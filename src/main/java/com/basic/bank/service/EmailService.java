489d70862cd5c6d0a11d011e9b1ac78c73e29968
Sarthak Shrivastava <161567581+SarthakShrivastav-a@users.noreply.github.com>
2025-02-07 23:03:12 +0530

    Async Notations

--- src/main/java/com/basic/bank/service/EmailService.java
+++ src/main/java/com/basic/bank/service/EmailService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +17,7 @@ public class EmailService {
     @Value("${spring.mail.username}")
     private String from;
 
+    @Async
     public void sendEmail(String recipient, String body, String subject){
 
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
